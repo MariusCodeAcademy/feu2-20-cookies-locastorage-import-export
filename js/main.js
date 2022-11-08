9c0a7fee9a3445680b34e8480e4b0b42cbfb97f2
Marius <marius@example.com>
2022-11-08 09:01:38 +0200

    local

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
 'use strict';
 console.log('main.js');
+
+const set1El = document.getElementById('set1');
+const get1El = document.getElementById('get1');
+
+// const user1Id = 'u1234';
+
+set1El.addEventListener('click', () => {
+  console.log('paspaudei');
+  // localStorage.setItem('user', user1Id);
+});
+
+get1El.addEventListener('click', () => {
+  // iskonsolinti user1Id
+});
